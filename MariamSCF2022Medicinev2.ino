7ea727c866b4ad53ae56598498ac99d1311ecfe3
mariamiqbal2025 <100160020+mariamiqbal2025@users.noreply.github.com>
2022-02-25 22:50:25 -0500

    Add files via upload

--- MariamSCF2022Medicinev2.ino
+++ MariamSCF2022Medicinev2.ino
@@ -13,8 +13,8 @@ Advanced Math and Science Academy Charter School In Marlborough
   IR Sensor connection 
 PEMENOL CHIP CONFIGURATION:
 pin 100 song number 1001.mp3 "Please take medication" connected to pin 5 on MKR1000 main chip 
-pin 102 song number 1003.mp3 "Medication dispensed" connected to pin 4 on MKR1000 main chip
-pin 104 song number 1005.mp3 "Medication picked up" connected to pin 3 on MKR1000 main chip
+pin 102 song number 1003.mp3 "Medication dispensed" connected to pin 3 on MKR1000 main chip
+pin 104 song number 1005.mp3 "Medication picked up" connected to pin 4 on MKR1000 main chip
   
 */
 
@@ -44,45 +44,53 @@ unsigned long int day=86400000; //milliseconds in a day
 unsigned long int hour=3600000; //milliseconds in a hour
 unsigned long int minute=60000; //milliseconds in a min
 unsigned long int second=1000; //milliseconds in a second
-
 int overdoseProtection=0; // is the tray not empty? 
-unsigned long int timeInterval = 5; // 5 minutes interval for medication dosage, how often you should take the medication
-unsigned long int timeCheck= 1; // 1min  after timeCheck hrs, check if medication was taken 
-unsigned long int lastDoseTime= 0; // last time dose was dispensed
-
-boolean voicealert= false; // starts the program with the voice alert off 
+ int timeInterval = 2; // 2 minutes interval for medication dosage, how often you should take the medication
+ int timeCheck= 1; // 1 min  after timeCheck hrs, check if medication was taken 
+ int lastDoseTime= 0; // last time dose was dispensed
+//boolean voicealert= false; // starts the program with the voice alert off 
 int count =0 ; // count # of times voiceAlert was used
 int maxCount=3; //maximum amount of times voiceAlert will happen before a text message will be sent to loved ones. 
-
 int timelapsedinmins =0; // time lapsed in minutes 
 
-void setup() {
-  delay(10000); //delay to show the console display
- 
-
-  int r= Connect_to_Wifi(); // Connect to wifi for sendign text message 
-  
-  lastDoseTime= timeprint(millis()); // initializes the var to the time when you start the program 
-  Serial.println("Start time:");
-  Serial.println(lastDoseTime);
-  /*
+int PIN_M1_Motor =6; // attaches the servo on pin 6 to the servo object Motor 1
+int PIN_M2_Motor =7 ; //attaches the servo on pin 7to the servo object Motor 2
+int PIN_IR_Sensor1 = 9; // pin 9 is connected to the IR sensor output
+ /*
    * pin 100 song number 1001.mp3 "Please take medication" connected to pin 5 on MKR1000 main chip 
     pin 102 song number 1003.mp3 "Medication dispensed" connected to pin 4 on MKR1000 main chip
     pin 104 song number 1005.mp3 "Medication picked up" connected to pin 3 on MKR1000 main chip
    * 
-   */
-  pinMode(9, INPUT); // pin 9 is connected to the IR sensor output
-  pinMode(5, OUTPUT); // pin5 is connected to PEMENOL board for voice alert audio
-  pinMode(4, OUTPUT); // pin4 is connected to PEMENOL board for voice alert audio
-  pinMode(3, OUTPUT); // pin3 is connected to PEMENOL board for voice alert audio
+ */
+int PIN_VM_PTM =5;
+int PIN_VM_MD =3;
+int PIN_VM_MPU =4; 
+
+void setup() {
+  delay(10000); //delay to show the console display
+  int r= Connect_to_Wifi(); // Connect to wifi for sendign text message 
+   
+  lastDoseTime= timeprint(millis()); // initializes the var to the time when you start the program 
+  Serial.println("Start time:");
+  Serial.println(lastDoseTime);
+ 
+  pinMode(PIN_IR_Sensor1, INPUT); // pin 9 is connected to the IR sensor output
+  pinMode(PIN_VM_PTM, OUTPUT); // pin5 is connected to PEMENOL board for voice alert audio
+  pinMode(PIN_VM_MD, OUTPUT); // pin4 is connected to PEMENOL board for voice alert audio
+  pinMode(PIN_VM_MPU, OUTPUT); // pin3 is connected to PEMENOL board for voice alert audio
   
   //delay(10000); // Delay 10 seconds 
-  M1.attach(6);  // attaches the servo on pin 6 to the servo object Motor 1
-   M2.attach(7);  // attaches the servo on pin 7to the servo object Motor 2
+   M1.attach(PIN_M1_Motor);  // attaches the servo on pin 6 to the servo object Motor 1
+   M2.attach(PIN_M2_Motor);  // attaches the servo on pin 7to the servo object Motor 2
    M1.write(0);
+   delay(1000);
    M2.write(0);
-   digitalWrite(5,HIGH);// pin5 is connected to PEMENOL board for voice alert audio; initialize to no voice;
-     delay(10000); // Delay 10 seconds 
+   digitalWrite(PIN_VM_PTM,HIGH);// pin5 is connected to PEMENOL board for voice alert audio; initialize to no voice;
+   digitalWrite(PIN_VM_MD,HIGH);// pin4 is connected to PEMENOL board for voice alert audio; initialize to no voice;
+   digitalWrite(PIN_VM_MPU,HIGH);// pin3 is connected to PEMENOL board for voice alert audio; initialize to no voice;
+   delay(1000); // Delay 1 seconds 
+ 
+    
 }
 
 void loop() {
@@ -94,15 +102,10 @@ void loop() {
 if(overdoseProtection==0 ){ 
   // release medications 
   releaseDose(); 
-  //record the time when the dose was released. 
-  
-  Serial.println("Net Dose  time: ");
-  lastDoseTime=timeprint(millis()); 
-  
   count=0; 
 }
-delay(1000);
-  //delay(60000); // Delay a minute
+else{
+delay(1000);   // Delay a secons
 // read obstacle IR sensor
 readIRSensor();  
 timelapsedinmins=timeprint(millis())-lastDoseTime; 
@@ -111,30 +114,21 @@ Serial.println(timelapsedinmins);
 
 if(overdoseProtection==1 && ((timelapsedinmins>=timeCheck) || count>=1)){
   voiceAlert();
-  Serial.println("Waiting after voice alert for 30seconds"); 
-  delay(30000);
+  Serial.println("Waiting after voice alert for 10seconds"); 
+  delay(10000);
   if(count>maxCount){
     sendtextMessage(); 
   }
 }
 
-
-
-
-
-
-
-
-
-
-  
+}
 }
 
 void releaseDose(){
   timelapsedinmins=timeprint(millis())-lastDoseTime; 
   Serial.print("Release dose : Time elapsed in min:");
-  Serial.println(timelapsedinmins);
-   if((timelapsedinmins-timeInterval<=0) || count==0){
+  Serial.println(timelapsedinmins-timeInterval);
+   if(timelapsedinmins-timeInterval>=0) {
       M1.write(0);                  // Rotate 90 degree
       delay(1000);                           // waits for the servo to get there
       M1.write(90);
@@ -148,13 +142,15 @@ void releaseDose(){
       overdoseProtection=1; //there is something on the tray, activate overdoseProtection
       Serial.println("Medicine dispensed"); 
       Medicationdispensed();
-       
+       //record the time when the dose was released. 
+      Serial.println("Last Dose  time: ");
+      lastDoseTime=timeprint(millis()); // Reset and print the last dose time 
    }
    else 
    Serial.println("Medicine not dispensed yet."); 
 }
 void readIRSensor(){
-  if(digitalRead(9)==LOW){
+  if(digitalRead(PIN_IR_Sensor1)==LOW){
       Serial.println("Medicine is still on the tray");
       overdoseProtection=1;
   
@@ -163,6 +159,7 @@ void readIRSensor(){
     Serial.println("Medicine picked up!");
     Medicationpickedup();
     overdoseProtection=0; 
+    lastDoseTime= timeprint(millis()); //Reset the last dose time
   }  
 
 
@@ -171,10 +168,10 @@ void readIRSensor(){
 }
 void voiceAlert(){
 
-    digitalWrite(5,LOW); // pin 5 is configured to the speaker system + playback chip 
+    digitalWrite(PIN_VM_PTM,LOW); // pin 5 is configured to the speaker system + playback chip 
     delay(1000);
-    digitalWrite(5,HIGH);
-    Serial.print("VOICE ALERT ACTIVATED : "); 
+    digitalWrite(PIN_VM_PTM,HIGH);
+    Serial.print("Voice Alert Activated: "); 
     Serial.println(count);
     count++; 
     
@@ -184,9 +181,9 @@ void voiceAlert(){
 
 void Medicationdispensed(){
 
-    digitalWrite(4,LOW); // pin 4 is configured to the speaker system + playback chip 
+    digitalWrite(PIN_VM_MD,LOW); // pin 4 is configured to the speaker system + playback chip 
     delay(1000);
-    digitalWrite(4,HIGH);
+    digitalWrite(PIN_VM_MD,HIGH);
     Serial.print("Medication dispensed: ");  
     delay(10000);
 
@@ -194,9 +191,9 @@ void Medicationdispensed(){
 }
 void Medicationpickedup(){
 
-    digitalWrite(3,LOW); // pin 3 is configured to the speaker system + playback chip 
+    digitalWrite(PIN_VM_MPU,LOW); // pin 3 is configured to the speaker system + playback chip 
     delay(1000);
-    digitalWrite(3,HIGH);
+    digitalWrite(PIN_VM_MPU,HIGH);
     Serial.print("Medication picked up! ");  
     delay(10000);
 
